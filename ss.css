475b0644264ab23bb5aa869a60a029d95a6092cf
gitanjaliw <er.gitanjaliwagh@gmail.com>
2014-01-31 12:35:47 +0530

    Create ss.css

---
+++ ss.css
@@ -0,0 +1,10 @@
+.avatar-change:hover {
+    background-color: #EEEEEE;
+}
+.unanswered strong {
+    background: none repeat scroll 0 0 rgba(0, 0, 0, 0);
+    color: #9A4444 !important;
+}
+.post-tag.selected {
+    background-color: #C4DAE9;
+}
